d0f54bb78bd4bce17b25059e06d931d1cc1b97d8
RAIUTC <syi05003@gmail.com>
2024-12-19 20:18:06 +0900

    for debugging

--- code/model.py
+++ code/model.py
@@ -116,8 +116,8 @@ class LightGCN(BasicModel):
             self.embedding_item.weight.data.copy_(torch.from_numpy(self.config['item_emb']))
             print('use pretarined data')
         self.f = nn.Sigmoid()
-        print("ok")
         self.Graph = self.dataset.getSparseGraph()
+        print("ok")
         print(f"lgn is already to go(dropout:{self.config['dropout']})")
 
         # print("save_txt")
